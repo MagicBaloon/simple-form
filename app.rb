9dadfe1ba2093d768068d027999a891e7a19e1ee
= <d.byriansky@gmail.com>
2018-05-08 01:16:59 +0300

    Change controller app.rb

--- app.rb
+++ app.rb
@@ -8,8 +8,15 @@ end
 post '/' do
   @username = params[:username]
   @phone    = params[:phone]
+  @email    = params[:email]
   @topic    = params[:topic]
   @date     = params[:date]
   @time     = params[:time]
   @message  = params[:message]
+
+  @result = {}
+  @result[:title] = "Well done!"
+  @result[:message] = "#{@username}, we received your data and started processing :)"
+
+  erb :result
 end
