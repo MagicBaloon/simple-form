1e1d471044dae348e453ea8db1d303a3327a03b8
= <d.byriansky@gmail.com>
2018-05-08 02:22:08 +0300

    Change save data syntax, add admin area access

--- app.rb
+++ app.rb
@@ -19,8 +19,29 @@ post '/' do
   @result[:message] = "#{@username}, we received your data and started processing :)"
 
   @file = File.open('userdata.txt', 'a')
-  @file.write "[Username: #{@username} __ Phone: #{@phone} __ Email: #{@email} __ Topic: #{@topic} __ Date: #{@date} __ time: #{@time} __ message: #{@message}]"
+  @file.write "\n\r[ Username: #{@username} | Phone: #{@phone} | Email: #{@email} | Topic: #{@topic} | Date: #{@date} | Time: #{@time} | Message: #{@message} ]"
   @file.close
 
   erb :result
+end
+
+get '/admin' do
+  erb :login
+end
+
+post '/admin' do
+  @user = params[:user]
+  @password = params[:password]
+
+  if @user == 'admin' && @password == 'admin'
+    @file = File.open('userdata.txt', 'r')
+    @userdata = @file.read
+    @file.close
+
+    erb :admin
+  else
+    @message = "<h3>Wrong username or password! Access denied.</h3>"
+    
+    erb :login
+  end
 end
